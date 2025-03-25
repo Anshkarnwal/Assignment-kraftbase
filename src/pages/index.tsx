b1cbaf7ac46d7d84ec2cdb2bccbc500427bafef8
Ansh Karnwal <avianshkarnwal@gmail.com>
2025-03-26 00:13:48 +0530

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-// pages/index.tsx
-
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import FrameworkNodesAnimation from '../components/FrameworkNodesAnimation';
@@ -13,52 +11,63 @@ import MotionAnimation from '../components/MotionAnimation';
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gray-100">
       <Head>
         <title>Design System Animations</title>
         <meta name="description" content="Design System Hover Animations" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="max-w-7xl mx-auto">
-        <h1 className="text-3xl font-bold text-center mb-12">Design System Hover Animations</h1>
-        
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div>
-            <FrameworkNodesAnimation />
+      <main className="grid grid-cols-9 gap-4 p-2 h-full">
+        {/* Framework Section */}
+        <div className="col-span-2 bg-slate-800 rounded-lg p-4 text-white">
+          <FrameworkNodesAnimation />
+        </div>
+
+        {/* Voice & Tone Section */}
+        <div className="col-span-2 bg-amber-300 rounded-lg p-4">
+          <VoiceToneAnimation />
+        </div>
+
+        {/* Centered Blue Box */}
+        <div className="col-span-1 flex items-center justify-center">
+          <div className="bg-blue-600 rounded-lg w-full h-full flex items-center justify-center">
+            <p className="text-white font-bold">Centered Box</p>
           </div>
-          
-          <div>
-            <VoiceToneAnimation />
-          </div>
-          
-          <div>
-            <LogoAnimation />
-          </div>
-          
-          <div>
-            <TypographyAnimation />
-          </div>
-          
-          <div>
-            <IconographyAnimation />
-          </div>
-          
-          <div>
-            <ColorAnimation />
-          </div>
-          
-          <div>
-            <ImageryAnimation />
-          </div>
-          
-          <div>
-            <MotionAnimation />
-          </div>
+        </div>
+
+        {/* Logo Section */}
+        <div className="col-span-2 bg-cyan-400 rounded-lg p-4">
+          <LogoAnimation />
+        </div>
+
+        {/* Typography Section */}
+        <div className="col-span-2 bg-orange-500 rounded-lg p-4">
+          <TypographyAnimation />
+        </div>
+
+        {/* Iconography Section */}
+        <div className="col-span-2 bg-lime-400 rounded-lg p-4">
+          <IconographyAnimation />
+        </div>
+
+        {/* Color Section */}
+        <div className="col-span-2 bg-orange-400 rounded-lg p-4">
+          <ColorAnimation />
+        </div>
+
+        {/* Imagery Section */}
+        <div className="col-span-2 bg-purple-700 rounded-lg p-4 text-white">
+          <ImageryAnimation />
+        </div>
+
+        {/* Motion Section */}
+        <div className="col-span-2 bg-purple-300 rounded-lg p-4">
+          <MotionAnimation />
         </div>
       </main>
     </div>
   );
 };
 
-export default Home;
+export default Home;  
